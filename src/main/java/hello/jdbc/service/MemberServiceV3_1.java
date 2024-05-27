48a7aab7e2d01a799914c5d979e314d3bdda2ce4
NBguys <gudwns520@gamil.com>
2024-05-27 17:07:48 +0900

    스프링을 통한 트랙잭션 해결 1번째

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,67 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 repository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            //트랜잭션 시작
+
+            Member fromMember = repository.findById(fromId);
+            Member toMember = repository.findById(toId);
+
+            fromMember.setMoney(fromMember.getMoney() - money);
+            toMember.setMoney(toMember.getMoney() + money);
+
+            repository.update(fromMember);
+            vaildation(toMember);
+            repository.update(toMember);
+
+            transactionManager.commit(status);
+
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); //커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static void vaildation(Member toMember) {
+        if (toMember.getMemberId().equals("memberEX")) {
+            throw new IllegalStateException();
+        }
+    }
+
+}
