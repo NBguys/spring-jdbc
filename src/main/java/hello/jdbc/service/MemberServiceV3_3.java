1c5256248b089a33c44627f78c210cfcffebaac4
NBguys <gudwns520@gamil.com>
2024-05-27 22:23:21 +0900

    트랜잭션 AOP 적용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,42 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 repository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = repository.findById(fromId);
+        Member toMember = repository.findById(toId);
+
+        fromMember.setMoney(fromMember.getMoney() - money);
+        toMember.setMoney(toMember.getMoney() + money);
+        repository.update(fromMember);
+        vaildation(toMember);
+        repository.update(toMember);
+    }
+
+    private static void vaildation(Member toMember) {
+        if (toMember.getMemberId().equals("memberEX")) {
+            throw new IllegalStateException();
+        }
+    }
+
+}
